d26f9198cefd8835527ecf14495e7b18d5416268
Walter Schmidt Silva <walter@tecacademy.com.br>
2020-05-10 21:05:28 -0300

    Codado até a aula: Implementação de Conexão, Sessão e Usuário

--- libpythonpro/tests/test_spam/test_enviador_de_email.py
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -1,15 +1,39 @@
-from libpythonpro.spam.enviador_de_email import Enviador
+import pytest
+
+from libpythonpro.spam.enviador_de_email import Enviador, EmailInvalido
+
 
 def test_criar_enviador_de_email():
     enviador = Enviador()
     assert enviador is not None
 
-def test_remetente():
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['walter@python.pro.br', 'foo@bar.com.br']
+)
+def test_remetente(remetente):
     enviador = Enviador()
+
     resultado = enviador.enviar(
-        'walter@python.pro.br',
+        remetente,
         'renzo@python.pro.br',
         'Curso PyTools',
         'Codando as aulas do curso do PyTools'
     )
-    assert 'walter@python.pro.br' in resultado
+    assert remetente in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['', 'foo']
+)
+def test_remetente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(
+            remetente,
+            'renzo@python.pro.br',
+            'Curso PyTools',
+            'Codando as aulas do curso do PyTools'
+        )
