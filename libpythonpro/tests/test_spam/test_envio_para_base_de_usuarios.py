fe46bdb1c3692759491c37121d33d9e821105f5b
Walter Schmidt Silva <walter@tecacademy.com.br>
2020-05-24 12:24:26 -0300

    Estudado e criado a classe Mock para a injeção de dependência para apenas no momento dos testes trocar um componente por outro para os testes.

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -21,7 +21,7 @@ from libpythonpro.spam.modelos import Usuario
 def test_qde_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = EnviadorMock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'w.schmidt@uol.com.br',
@@ -29,3 +29,34 @@ def test_qde_de_spam(sessao, usuarios):
         'Confira os módulos fantásticos'
     )
     assert len(usuarios) == enviador.qtd_email_enviados
+
+
+class EnviadorMock(Enviador):
+
+    def __init__(self):
+        super().__init__()
+        self.qtd_email_enviados = 0
+        self.paramentros_de_envio = None
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.paramentros_de_envio = (remetente, destinatario, assunto, corpo)
+        self.qtd_email_enviados += 1
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Renzo', email='renzo@python.pro.br')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'w.schmidt@uol.com.br',
+        'Curso Python Pro,',
+        'Confira os módulos fantásticos'
+    )
+
+    assert enviador.paramentros_de_envio == (
+        'w.schmidt@uol.com.br',
+        'renzo@python.pro.br',
+        'Curso Python Pro,',
+        'Confira os módulos fantásticos'
+    )
