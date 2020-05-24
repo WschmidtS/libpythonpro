c21930ebb43bbf53731e0cb058ee707f4cb04a40
Walter Schmidt Silva <walter@tecacademy.com.br>
2020-05-24 14:14:17 -0300

    Empregado o conceito de fixture para isolar um teste do outro. Implementado o teste de integração no github_api

--- libpythonpro/tests/test_github_api.py
+++ libpythonpro/tests/test_github_api.py
@@ -1,14 +1,27 @@
 from unittest.mock import Mock
 
+import pytest
+
 from libpythonpro import github_api
 
 
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url():
     resp_mock = Mock()
+    url = 'https://avatars2.githubusercontent.com/u/63677631?v=4'
     resp_mock.json.return_value = {
         'login': 'WschmidtS', 'id': 63677631,
-        'avatar_url': 'https://avatars2.githubusercontent.com/u/63677631?v=4',
+        'avatar_url': url,
     }
+    get_original = github_api.requests.get
     github_api.requests.get = Mock(return_value=resp_mock)
-    url = github_api.buscar_avatar('WschmidtS')
-    assert 'https://avatars2.githubusercontent.com/u/63677631?v=4' == url
+    yield url
+    github_api.requests.get = get_original
+
+def test_buscar_avatar(avatar_url):
+   url = github_api.buscar_avatar('WschmidtS')
+   assert avatar_url == url
+
+def test_buscar_avatar_integracao():
+   url = github_api.buscar_avatar('WschmidtS')
+   assert 'https://avatars2.githubusercontent.com/u/63677631?v=4' == url
