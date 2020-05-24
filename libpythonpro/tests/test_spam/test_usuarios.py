5660b66b7046ce644b105b638c2cd62e570c1015
Walter Schmidt Silva <walter@tecacademy.com.br>
2020-05-24 11:34:19 -0300

    Alterado o código de produção de Enviador() para poder testar a quantidade de emails enviados.

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -2,13 +2,16 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Walter')
+    usuario = Usuario(nome='Walter', email='w.schmidt@uol.com.br')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
-    usuarios = [Usuario(nome='Walter'), Usuario(nome='Renzo')]
+    usuarios = [
+        Usuario(nome='Walter', email='w.schmidt@uol.com.br'),
+        Usuario(nome='Renzo', email='renzo@python.pro.br')
+    ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
